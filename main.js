23de1860cd36e37eece9d13540efb1dbcaff4b22
erkaerka21 <eegiibaagii597@gmail.com>
2024-06-13 12:48:19 +0800

    6.13 array

---
+++ main.js
@@ -0,0 +1,25 @@
+let X = [12, 34, 56, 78, 90];
+console.log(X[3]);
+
+let Y = [
+  [30, 40, 50],
+  [25, 35, 45],
+  [15, 98, 76],
+];
+console.log(Y[1][2]);
+
+//daalgavar1
+let fruits = ["apple", "banana", "orange", "kiwi", "grape"];
+console.log("жимснүүд : ", fruits);
+console.log(fruits[0]);
+console.log(fruits[4]);
+console.log(fruits[2]);
+
+//daalgavar2
+let too = [5, 6, 4, 12, 19, 121, 1, 7, 9, 63];
+console.log("тоонууд : ", too);
+if (too % 2 === 0) {
+  console.log("тэгш тоонууд : ", too);
+} else {
+  console.log("сондгой тоонууд : ", too);
+}
